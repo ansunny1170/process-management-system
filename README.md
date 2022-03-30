14f7638aa3acd1459fd457eb8c08e87b849e0318
Yechan Kim <cjstkdpcks10@naver.com>
2022-03-30 21:04:29 +0900

    backend code update

---
+++ README.md
@@ -0,0 +1,16 @@
+# hehe-back
+npm install express-generator -g <br>
+npm install <br>
+npm install nodemon -g <br>
+npm install -g eslint <br>
+npm install dotenv --save <br>
+npm install body-parser --save <br>
+npm install winston --save <br>
+npm install winston-daily-rotate-file --save <br>
+npm install cors --save <br>
+npm install pg pg-hstore --save <br>
+npm install sequelize --save <br>
+npm install crypto <br>
+npm install jsonwebtoken --save <br>
+npm install mqtt <br>
+npm install influx <br>
---
+++ backend/models/index.js
@@ -0,0 +1,45 @@
+const { sequelize } = require('./connection');
+const User = require('./user');
+const Machine = require('./machine');
+const Management = require('./management');
+const Item = require('./item');
+const EmoHistory = require('./emoHistory');
+const MachineHistory = require('./machineHistory');
+const Order = require('./order');
+const WorkStatus = require('./workStatus');
+
+const db = {};
+
+db.sequelize = sequelize;
+
+// model 생성
+db.User = User;
+db.Machine = Machine;
+db.Management = Management;
+db.Item = Item;
+db.EmoHistory = EmoHistory;
+db.MachineHistory = MachineHistory;
+db.Order = Order;
+db.WorkStatus = WorkStatus;
+
+// model init
+User.init(sequelize);
+Machine.init(sequelize);
+Management.init(sequelize);
+Item.init(sequelize);
+EmoHistory.init(sequelize);
+MachineHistory.init(sequelize);
+Order.init(sequelize);
+WorkStatus.init(sequelize);
+
+// association(관계 생성)
+User.associate(db);
+Machine.associate(db);
+Management.associate(db);
+Item.associate(db);
+EmoHistory.associate(db);
+MachineHistory.associate(db);
+Order.associate(db);
+WorkStatus.associate(db);
+
+module.exports = db;
